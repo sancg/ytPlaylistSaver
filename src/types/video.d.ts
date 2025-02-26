d36d6a97f00ae04f5b01a1ce323890375556d105
Santiago Cano <sanblackx1@gmail.com>
2025-02-26 15:52:23 -0500

    ADD: video types

---
+++ src/types/video.d.ts
@@ -0,0 +1,11 @@
+type Video = {
+  id: string | null;
+  title: string | null;
+  url: string | null;
+  thumbImg?: string;
+  publishedBy?: string;
+  timeLength?: string;
+  currentIndex?: boolean;
+};
+
+export { Video };
