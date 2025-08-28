12d99a513148d69f78aa7cf9309b207d9a2611e6
Santiago Cano <sanblackx1@gmail.com>
2025-08-28 14:53:18 -0400

    NEW: Playlist viewer setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       input: {
         index: '/index.html',
         contentScript: '/contentScript.ts',
+        viewer: '/playlist_viewer.html',
       },
       output: {
         entryFileNames: 'assets/app/[name].js',
