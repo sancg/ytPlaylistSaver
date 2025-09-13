9d76c3bd0f26451a5b01f30a0e1494f54ea42cbb
topduh <sanblackx1@gmail.com>
2025-09-13 01:23:20 -0500

    CHORE: Recap on Iframe YT

--- src/pages/PlaylistViewer/PlaylistViewer.tsx
+++ src/pages/PlaylistViewer/PlaylistViewer.tsx
@@ -74,8 +74,8 @@ export const PlaylistViewer: React.FC = () => {
   };
 
   return (
-    <main className='bg-yt-bg w-full p-3'>
-      <div className='flex flex-col h-(--yt-screen) gap-2 lg:flex-row'>
+    <main className='bg-yt-bg w-full p-4'>
+      <div className='flex flex-col h-(--yt-screen) gap-4 lg:flex-row'>
         <div className='w-full min-h-[600px] flex items-center justify-center bg-yt-bg rounded-xl border border-yt-bg-secondary'>
           <Player video={currentVideo} />
         </div>
