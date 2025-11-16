9048ac2d20ebd68534db854011a212da8de8884d
topduh <sanblackx1@gmail.com>
2025-11-16 00:39:32 -0500

    ADD: the App.tsx is entrypoint for te popup - now it opens side_panel

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,22 +1,31 @@
-import { ArrowLeftEndOnRectangleIcon } from '@heroicons/react/20/solid';
+import { cs } from '../../scripts/shared/constants';
+import { sendToBackground } from '../../utils/actions/messages';
 
 export const Popup = () => {
   const openSidePanel = async () => {
-    const { id } = await chrome.windows.getCurrent();
-    await chrome.sidePanel.setOptions({
-      path: 'src/pages/side_panel/side-panel.html',
-      enabled: true,
-      tabId: id,
-    });
-    await chrome.sidePanel.open({ windowId: id! });
+    // FIXME: Related to the side_panel view - TODO - only show in yt tabs
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    if (tab.id) await sendToBackground({ type: cs.OPEN_PANEL, payload: { id: tab.id } });
   };
+
+  // const renderIconSP = () => {
+  //   return (
+  //     <div className='p-2' title='Open SidePanel'>
+  //       <ArrowLeftEndOnRectangleIcon
+  //         className='transition-all duration-300 ease-in-out hover:scale-125 hover:cursor-pointer'
+  //         width={20}
+  //         onClick={openSidePanel}
+  //       />
+  //     </div>
+  //   );
+  // };
   return (
-    <div className='p-2' title='Open SidePanel'>
-      <ArrowLeftEndOnRectangleIcon
-        className='transition-all duration-300 ease-in-out hover:scale-125 hover:cursor-pointer'
-        width={20}
-        onClick={openSidePanel}
-      />
-    </div>
+    <button
+      //bg-[#e1002d]
+      className='bg-yt-accent-red font-bold hover:cursor-pointer hover:bg-red-700 px-3 py-2 rounded-2xl w-full'
+      onClick={openSidePanel}
+    >
+      Open Playlist Viewer
+    </button>
   );
 };
