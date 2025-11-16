2699b043f80c47e4b13305f43c4604cd978ef16f
topduh <sanblackx1@gmail.com>
2025-11-16 00:45:27 -0500

    CHORE: constants.ts + removed comments

---
+++ src/scripts/shared/constants.ts
@@ -0,0 +1,12 @@
+export const cs = {
+  ORIGIN: 'youtube.com/watch',
+  EXTRACT_PLAYLIST: 'extract_playlist',
+  ADD_VIDEO: 'add_video',
+  IS_SAVED: 'is_saved',
+  GET_VIDEOS: 'get_videos',
+  AVAILABLE_LIST: 'get_keys',
+  REMOVE_VIDEO: 'remove_video',
+  OPEN_PANEL: 'open_local_panel',
+} as const;
+
+export type MessageType = (typeof cs)[keyof typeof cs];
